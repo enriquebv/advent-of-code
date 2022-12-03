f953630638bc0f3c9d7a4cafe67131decf18a2f2
Enrique Bernabeu <ebvcontacto@gmail.com>
2022-12-03 17:15:39 +0100

    Enhanced day 3 code communication

--- javascript/day-03/index.js
+++ javascript/day-03/index.js
@@ -15,8 +15,13 @@ export function getItemValue(char) {
   return charCode - 38
 }
 
-export const getFirstCharacterCoincidenceInCollections = (...collections) =>
-  collections[0].find((char) => collections.slice(1).every((collection) => collection.includes(char)))
+export function getCharacterCoincidenceBetweenCollections(...collections) {
+  const [reference, ...otherCollections] = collections
+
+  return reference.find((character) => {
+    return otherCollections.every((collection) => collection.includes(character))
+  })
+}
 
 export function solvePartOne(rucksacks) {
   let totalPriorities = 0
@@ -25,7 +30,7 @@ export function solvePartOne(rucksacks) {
     const first = rucksack.slice(0, rucksack.length / 2)
     const second = rucksack.slice(rucksack.length / 2)
 
-    const itemInBothCompartments = getFirstCharacterCoincidenceInCollections(first, second)
+    const itemInBothCompartments = getCharacterCoincidenceBetweenCollections(first, second)
     const itemValue = itemInBothCompartments ? getItemValue(itemInBothCompartments) : 0
 
     totalPriorities += itemValue
@@ -42,7 +47,7 @@ export function solvePartTwo(rucksacks) {
     const sliceStart = groupIndex * 3
     const sliceEnd = sliceStart + 3
     const group = rucksacks.slice(sliceStart, sliceEnd)
-    const badge = getFirstCharacterCoincidenceInCollections(...group)
+    const badge = getCharacterCoincidenceBetweenCollections(...group)
 
     totalPriorities += getItemValue(badge)
   }
