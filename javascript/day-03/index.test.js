778e68e7694b716215a7f2d3d2d17280d7d97de2
Enrique Bernabeu <ebvcontacto@gmail.com>
2022-12-03 14:05:15 +0100

    Added missing test in part 1, day 3

--- javascript/day-03/index.test.js
+++ javascript/day-03/index.test.js
@@ -1,5 +1,5 @@
 import { readFile } from '../shared'
-import { solve, parseInput } from './'
+import { solve, parseInput, getItemValue } from './'
 
 describe('Day 03', () => {
   it('should "parseInput" parse input into valid matrix data structure', () => {
@@ -13,6 +13,22 @@ describe('Day 03', () => {
       rucksack[0].forEach((item) => expect(typeof item).toBe('string'))
       rucksack[1].forEach((item) => expect(typeof item).toBe('string'))
     })
+  })
+
+  it('should "getItemValue" should return correct item values per character', () => {
+    expect(getItemValue('a')).toBe(1)
+    expect(getItemValue('b')).toBe(2)
+    expect(getItemValue('c')).toBe(3)
+    expect(getItemValue('x')).toBe(24)
+    expect(getItemValue('y')).toBe(25)
+    expect(getItemValue('z')).toBe(26)
+
+    expect(getItemValue('A')).toBe(27)
+    expect(getItemValue('B')).toBe(28)
+    expect(getItemValue('C')).toBe(29)
+    expect(getItemValue('X')).toBe(50)
+    expect(getItemValue('Y')).toBe(51)
+    expect(getItemValue('Z')).toBe(52)
   })
 
   it('should "solve" return expected result', () => {
